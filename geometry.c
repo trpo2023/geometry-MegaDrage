4e87608aa1e54b218f3f3e9f355dbd6ec8c1ef5e
Дмитрий Жеребцов <dragonpsui@gmail.com>
2023-03-30 13:19:28 +0700

    Realisation of entry in geometry.c

--- geometry.c
+++ geometry.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+char *err_msg; //error message
+int err_smb; //symbol of error
+int find_circle(char *str);
 
 int main(int argc, char* argv[])
 {
@@ -8,30 +13,260 @@ int main(int argc, char* argv[])
         printf("You have not entered FILE\n");
         return 1;
     }
-    FILE* ptr;
-    ptr = fopen(argv[1], "r");
-    if (ptr == NULL) {
+    FILE* file;
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
         printf("Couldn't open the FILE\n");
         return 2;
     }
-    char* figure_type = malloc(7 * sizeof(char));
-    fgets(figure_type, 7, ptr);
-    if (figure_type == NULL) {
-        printf("Figure type is dead\n");
-        return 3;
+    char str[100 + 1];
+    fgets(str, 100, file);
+    printf("%s", str);
+    int s;
+    int found = find_circle(str);
+    if(found == 0) {
+            printf("\nRight entry");
+            fclose(file);
+            printf("\nFile closed\n");
+            printf("Exit program...\n");
+            return 0;
+        } 
+        else {
+        for(s = 0; s < err_smb; s++)
+            printf(" ");
+        printf("^\n");
+        printf("Error at column %d: %s", err_smb + 1, err_msg);
+        fclose(file);
+        printf("\nFile closed\n");
+        printf("Exit program with error\n");
+        return 1;
     }
-    /*for (int i = 0; i < strlen(figure_type); i++) {
-        printf("%c\n", figure_type[i]);
-    }*/
-    char* point = malloc(6 * sizeof(char));
-    fgets(point, 5, ptr);
-    int points [2];
-    char x = point[1];
-    char y = point[3];
-    points[0] = atoi(x);
-    points[1] = atoi(y);
-    printf("x: %i, y: %i\n", points[0], points[1]);
-    free(figure_type);
-    free(point);
+    return 0;
+}
+
+int find_circle(char *str) {
+    int i, j;
+
+    if(strncmp(str, "circle(", 7) == 0) { // circle
+
+        for(i = 7; i < strlen(str); i++) { //founding number 1
+            if(str[i] == 32) { // " "
+                    continue;
+                }
+            if(isdigit(str[i])) {
+                for(j = i + 1; j < strlen(str); j++) {
+                    if(isdigit(str[j])) {
+                        continue;
+                    }
+                    if(str[j] == 32) { // " "
+                        break;
+                    }
+                    if(str[j] == 46) { // "."
+                        break;
+                    }
+                    err_msg = "expected digit, dot or space\n";
+                    err_smb = j;
+                    return 1;
+                }
+                if(str[j] == 0) {
+                    err_msg = "expected digit, dot or space\n";
+                    err_smb = j;
+                    return 1;
+                }
+            } else {
+                err_msg = "expected number or space\n";
+                err_smb = i;
+                return 1;
+            }
+            if(str[j] == 46) {
+                for(j++; j < strlen(str); j++) {
+                        if(isdigit(str[j])) {
+                            continue;
+                        }
+                        if(str[j] == 32) { // " "
+                            break;
+                        }
+                        err_msg = "expected digit or space\n";
+                        err_smb = j;
+                        return 1;
+                }
+            }
+            if(str[j] == 0) {
+                err_msg = "expected digit or space\n";
+                err_smb = j;
+                return 1;
+            }
+            break;
+        }
+        if(str[i] == 0) {
+            err_msg = "expected number\n";
+            err_smb = i;
+            return 1;
+        }
+        
+        for(i = j + 1; i < strlen(str); i++) { //founding number 2
+            if(str[i] == 32) { // " "
+                    continue;
+                }
+            if(isdigit(str[i])) {
+                for(j = i + 1; j < strlen(str); j++) {
+                    if(isdigit(str[j])) {
+                        continue;
+                    }
+                    if(str[j] == 44) { // ","
+                        break;
+                    }
+                    if(str[j] == 32) { // " "
+                        break;
+                    }
+                    if(str[j] == 46) { // "."
+                        break;
+                    }
+                    err_msg = "expected digit, dot, space or comma\n";
+                    err_smb = j;
+                    return 1;
+                }
+                if(str[j] == 0) {
+                    err_msg = "expected digit, dot, space or comma\n";
+                    err_smb = j;
+                    return 1;
+                }
+            } else {
+                err_msg = "expected number or space\n";
+                err_smb = i;
+                return 1;
+            }
+            if(str[j] == 46) {
+                for(j++; j < strlen(str); j++) {
+                        if(isdigit(str[j])) {
+                            continue;
+                        }
+                        if(str[j] == 44) { // ","
+                            break;
+                        }
+                        if(str[j] == 32) { // " "
+                            break;
+                        }
+                        err_msg = "expected digit, space or comma\n";
+                        err_smb = j;
+                        return 1;
+                }
+            }
+            if(str[j] == 0) {
+                err_msg = "expected digit, space or comma\n";
+                err_smb = j;
+                return 1;
+            }
+            break;
+        }
+        if(str[i] == 0) {
+            err_msg = "expected number\n";
+            err_smb = i;
+            return 1;
+        }
+        for(; j < strlen(str); j++) {
+            if(str[j] == 44) { // ","
+                break;
+            }
+            if(str[j] == 32) { // " "
+                continue;
+            }
+            err_msg = "expected space or comma\n";
+            err_smb = j;
+            return 1;
+        }
+        if(str[j] == 0) {
+            err_msg = "expected comma\n";
+            err_smb = j;
+            return 1;
+        }
+        for(i = j + 1; i < strlen(str); i++) { //founding number 3
+            if(str[i] == 32) { // " "
+                    continue;
+                }
+            if(isdigit(str[i])) {
+                for(j = i + 1; j < strlen(str); j++) {
+                    if(isdigit(str[j])) {
+                        continue;
+                    }
+                    if(str[j] == 32) { // " "
+                        break;
+                    }
+                    if(str[j] == 46) { // "."
+                        break;
+                    }
+                    if(str[j] == 41) { // ")"
+                        break;
+                    }
+                    err_msg = "expected digit, dot, space or bracket\n";
+                    err_smb = j;
+                    return 1;
+                }
+                if(str[j] == 0) {
+                    err_msg = "expected digit, dot, bracket or space\n";
+                    err_smb = j;
+                    return 1;
+                }
+            } else {
+                err_msg = "expected number or space\n";
+                err_smb = i;
+                return 1;
+            }
+            if(str[j] == 46) {
+                for(j++; j < strlen(str); j++) {
+                    if(isdigit(str[j])) {
+                        continue;
+                    }
+                    if(str[j] == 32) { // " "
+                        break;
+                    }
+                    if(str[j] == 41) { // ")"
+                        break;
+                    }
+                    err_msg = "expected digit, space or bracket\n";
+                    err_smb = j;
+                    return 1;
+                    }
+            }
+            if(str[j] == 0) {
+                err_msg = "expected digit, bracket or space\n";
+                err_smb = j;
+                return 1;
+            }
+            break;
+        }
+        if(str[i] == 0) {
+            err_msg = "expected number\n";
+            err_smb = i;
+            return 1;
+        }
+        for(; j < strlen(str); j++) {
+            if(str[j] == 32) { // " "
+                continue;
+            }
+            if(str[j] == 41) { // ")"
+                break;
+            }
+            err_msg = "expected space or bracket\n";
+            err_smb = j;
+            return 1;
+        }
+        if(str[j] == 0) {
+            err_msg = "expected bracket\n";
+            err_smb = j;
+            return 1;
+        }
+
+    } else {
+        if(strncmp(str, "circle", 6) == 0) {
+            err_msg = "expected '('\n";
+            err_smb = 6;
+            return 1;
+        } else {
+            err_msg = "expected 'circle'\n";
+            err_smb = 0;
+            return 1;
+        }
+    }
     return 0;
 }
